c6855eb805ff632c1df184201a1f26962ed4aeda
Colin Maykish <crmaykish@gmail.com>
2019-11-09 14:16:01 -0500

    Clear out old configs before adding new symlinks

--- setup_configs.sh
+++ setup_configs.sh
@@ -3,17 +3,22 @@
 SCRIPTS_DIR="${HOME}/Scripts"
 
 echo "Adding symlink for i3 config..."
+rm -rf ${HOME}/.config/i3/
 mkdir -p ${HOME}/.config/i3/
 ln -sv ${SCRIPTS_DIR}/dotfiles/i3_config ${HOME}/.config/i3/config
 
 echo "Adding symlink for i3status..."
+rm -rf ${HOME}/.config/i3status/
 mkdir -p ${HOME}/.config/i3status/
 ln -sv ${SCRIPTS_DIR}/dotfiles/i3status_config ${HOME}/.config/i3status/config 
 
 echo "Adding symlink for alacritty..."
+rm -rf ${HOME}/.config/alacritty
 mkdir -p ${HOME}/.config/alacritty
 ln -sv ${SCRIPTS_DIR}/dotfiles/alacritty.yml ${HOME}/.config/alacritty/alacritty.yml
 
 echo "Adding symlink for zsh..."
+rm -f ${HOME}/.zshrc
 ln -sv ${SCRIPTS_DIR}/dotfiles/.zshrc ${HOME}/.zshrc
 
+echo "Done! Reload your shell/desktop environment."
